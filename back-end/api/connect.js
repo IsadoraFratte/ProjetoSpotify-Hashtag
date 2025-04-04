faabf71c30836421fdd3ead2f7732ee24be26aeb
Isadora <isadorafratte@gmail.com>
2025-04-04 19:33:25 -0300

    pronto para deploy

---
+++ back-end/api/connect.js
@@ -0,0 +1,10 @@
+import { MongoClient } from 'mongodb';
+
+const URI = "mongodb+srv://isadoraimersao:pEqVi73ctECquDBl@cluster0.fg9fgo6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(URI);
+
+export const db = client.db("spotifyAula");
+
+// const teste = await db.collection("artists").find({}).toArray();
+// console.log(teste);
---
+++ back-end/api/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+import { db } from './connect.js';
+import path from 'path';
+
+const __dirname = path.resolve();
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+
+app.get("/api/", (req, res) => {
+  res.send("Welcome");
+});
+
+app.get("/api/artists", async (req, res) => {
+    res.send(await db.collection("artists").find({}).toArray());
+});
+
+app.get("/api/songs",  async (req, res) => {
+    res.send(await db.collection("songs").find({}).toArray());
+});
+
+app.use(express.static(path.join(__dirname, "../../front-end/dist")));
+
+app.get("*", async (req, res) => {
+  res.sendFile(path.join(__dirname,"../../front-end/dist/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
